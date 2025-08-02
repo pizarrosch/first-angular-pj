1dbb4677e912b27978747e227fbd4d2bedb1993b
pizarrosch <bayernsch89@gmail.com>
2025-08-03 01:37:50 +0200

    Refactor: Remove unused BookComponent import from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component, NgModule} from '@angular/core';
-import {BookComponent} from "./book/book/book.component";
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: false
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 
-export class AppComponent {}
+export class AppComponent {
+}
